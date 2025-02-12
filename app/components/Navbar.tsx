a1af3aac0aa2a920ee7ced59688a7f7cc36d2f7b
Mohammed Shafi Mundassery <shafimsm19@gmail.com>
2025-02-12 14:38:02 +0530

    added teh images

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -89,7 +89,7 @@ export function NavBar() {
               alt="Arbat Corporate Services"
               width={120}
               height={40}
-              className="h-10 w-auto"
+              className="h-16 w-auto"
             />
           </Link>
 
